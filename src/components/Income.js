a28aee467f3ed8d43443492492ac2334ca9c2e02
filipef9 <filipef9@hotmail.com>
2021-07-29 15:40:15 +0000

    Atualização da estilização do espaçamento da lista de rendimentos.

--- src/components/Income.js
+++ src/components/Income.js
@@ -8,7 +8,7 @@ const Income = ({ income }) => {
   const percentualStyle = (rendimentoPercentualIsNegative) ? 'text-red-700' : 'text-green-700';
 
   return (
-    <div className="flex mt-6 w-full">
+    <div className="flex mt-2 w-full">
       <div className="flex-1 text-sm font-medium leading-normal">{months[month]}/{year}</div>
       <div className="flex-1 text-sm font-medium leading-normal text-right">R$ {_.round(value, 2).toFixed(2)}</div>
       <div className={`flex-1 text-sm font-medium leading-normal text-right ${percentualStyle}`}>{_.round(rendimentoPercentual, 2).toFixed(2)}%</div>
