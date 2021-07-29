860aa4a8f514e17d8d3ce9b4738b9ece8549e4da
filipef9 <filipef9@hotmail.com>
2021-07-29 15:37:38 +0000

    Atualização da estilização do layout da lista de rendimentos.

--- src/components/Income.js
+++ src/components/Income.js
@@ -8,10 +8,10 @@ const Income = ({ income }) => {
   const percentualStyle = (rendimentoPercentualIsNegative) ? 'text-red-700' : 'text-green-700';
 
   return (
-    <div className="flex items-start justify-between mt-6 w-full">
-      <p className="text-sm font-medium leading-normal">{months[month]}/{year}</p>
-      <p className="text-sm font-medium leading-normal text-right">R$ {_.round(value, 2).toFixed(2)}</p>
-      <p className={`text-sm font-medium leading-normal text-right ${percentualStyle}`}>{_.round(rendimentoPercentual, 2).toFixed(2)}%</p>
+    <div className="flex mt-6 w-full">
+      <div className="flex-1 text-sm font-medium leading-normal">{months[month]}/{year}</div>
+      <div className="flex-1 text-sm font-medium leading-normal text-right">R$ {_.round(value, 2).toFixed(2)}</div>
+      <div className={`flex-1 text-sm font-medium leading-normal text-right ${percentualStyle}`}>{_.round(rendimentoPercentual, 2).toFixed(2)}%</div>
     </div>
   )
 }
