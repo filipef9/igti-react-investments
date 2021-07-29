ee7997db95f5e446db99911768b8096469c376ec
filipef9 <filipef9@hotmail.com>
2021-07-29 15:09:18 +0000

    Implementação do tratamento da cor do rendimento levando em consideração se foi negativo ou positivo.

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,13 +1,17 @@
-import { months } from '../helpers/dateHelpers';
 import _ from 'lodash';
+import { months } from '../helpers/dateHelpers';
 
 const Income = ({ income }) => {
   const { month, year, value, rendimentoPercentual } = income;
+
+  const rendimentoPercentualIsNegative = rendimentoPercentual < 0;
+  const percentualStyle = (rendimentoPercentualIsNegative) ? 'text-red-700' : 'text-green-700';
+
   return (
     <div className="flex items-start justify-between mt-6 w-full">
       <p className="text-sm font-medium leading-normal">{months[month]}/{year}</p>
       <p className="text-sm font-medium leading-normal text-right">R$ {_.round(value, 2).toFixed(2)}</p>
-      <p className="text-sm font-medium leading-normal text-right">{_.round(rendimentoPercentual, 2).toFixed(2)}%</p>
+      <p className={`text-sm font-medium leading-normal text-right ${percentualStyle}`}>{_.round(rendimentoPercentual, 2).toFixed(2)}%</p>
     </div>
   )
 }
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,6 +1,6 @@
 const Report = ({ fundName, rendimentoTotal, rendimentoPercentualTotal, children }) => {
   return (
-    <div className="py-8">
+    <div className="py-4">
       <div className="max-w-sm bg-white shadow rounded p-6">
         <div className="flex items-center justify-between">
           <p className="text-xl font-semibold leading-5 text-gray-800">{fundName}</p>
